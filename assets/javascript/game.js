787fdccdbc373ca8f0902b0e3f0a512baa228100
Dillon Faulkner <Kist221@Kist221-Mac.local>
2017-11-05 00:37:38 -0700

    updated psuedo code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,47 +2,47 @@
 
 // Variables needed for psychic game
     var gameChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var user = document.getElementById("user");
+    var guess = document.getElementById("guess");
     var win = document.getElementById("win");
     var lose = document.getElementById("lose");
-    var user = document.getElementById("user");
-    var comp = document.getElementById("comp");
+
     var cWin = 0
     var cLose = 0
-    var cTie = 0
+
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+
+
 
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
-      // Determines which key was pressed.
-      var userGuess = event.key;
+    // Determines which key was pressed.
+    var userGuess = event.key;
 
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
-      var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-
-      // only run upon valid input
-      if (   ) 
+      // Run if incorrect input
+      if (  userGuess === computerGuess ) 
       
       {
-      
-          // Displays User Input 
+          
+          // Display User Input 
           user.textContent = userGuess;
 
-          // Displays Comp Input
-          comp.textContent = computerGuess;
+          // adds 1 to win score
+          win.textContent = ++cWin;
 
-          // adds 1 to win scenario
-          if (    ) {
+          // reset scene
 
-            win.textContent = ++cWin;
+          // assign new random computer guess
+          var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+      }
 
-          }
+      // detract guess count and list new input
+      else {
 
-          // adds 1 to lose scenario
-          if (    ) {
+        // prevent duplicate input from affecting guess count
 
-            lose.textContent = ++cLose;
-
-          }
 
       }
 
